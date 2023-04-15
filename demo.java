72671f709a831b931a62bed765085a13c9b787a9
dnyaneshwari27 <dnyaneshwarighadge411@gmail.com>
2023-04-15 08:34:31 +0530

    added program

--- demo.java
+++ demo.java
@@ -2,11 +2,11 @@ class demo
 {
 	public static void main(String args[])
 	{
-		for(int i=0;i<5;i++)
+		for(int i=1;i<=5;i++)
 		{
-			for(int j=0;j<=i;j++)
+			for(int j=1;j<=i;j++)
 			{
-				System.out.println("*");
+				System.out.print("*");
 			}
 			System.out.println();
 		}
